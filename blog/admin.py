36e08224bc8f26ea6c98ce7390b81c9c1a477d43
Jonathan Farinloye <farinloyejonathan@gmail.com>
2019-06-21 00:23:18 +0100

    :construction: Models - Lesson 4

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'cover', 'slug')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    
+
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    raw_date = models.DateTimeField()
+    cover = models.ImageField(upload_to='images/')
+    content = models.TextField()
+    slug = models.SlugField()
+    
+    def date(self):
+        return self.raw_date.strftime('%b %d %Y')
+    
+    def __str__(self):
+        return self.title
